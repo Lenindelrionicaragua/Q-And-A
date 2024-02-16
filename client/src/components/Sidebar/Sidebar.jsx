5564feec0ba50c65038b668356fffb206c5171ec
lenin del rio <lenindelrionicaragua@gmail.com>
2024-02-16 18:51:12 +0100

    improve_sidebar_funtionality

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,59 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
 import PrimaryButton from "../Buttons/PrimaryButton";
 import ModulesList from "../ModulesList/ModulesList";
 import homeIcon from "../../images/icons/home-white.png";
-import homeIconHover from "../../images/icons/home.png";
+import homeIconHover from "../../images/icons/home-white.png";
 import modulesIcon from "../../images/icons/compass-white.png";
-import modulesIconHover from "../../images/icons/compass.png";
+import modulesIconHover from "../../images/icons/compass-white.png";
 import plusIcon from "../../images/icons/plus-white.png";
-import plusIconHover from "../../images/icons/plus.png";
+import plusIconHover from "../../images/icons/plus-white.png";
 
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   const [isModulesListVisible, setModulesListVisibility] = useState(false);
+  const [isSidebarVisible, setSidebarVisibility] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 960) {
+        setSidebarVisibility(false);
+        setModulesListVisibility(false);
+      } else {
+        setSidebarVisibility(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleModulesButtonClick = () => {
-    // Toggle the visibility of the Modules list when the button is clicked
     setModulesListVisibility(!isModulesListVisible);
   };
 
   return (
     <>
-      <nav className="site-nav" id="nav-sidebar">
-        <div className="nav-buttons">
-          <Link to="/">
+      {isSidebarVisible && (
+        <sidebar className="site-sidebar" id="nav-sidebar">
+          <div className="sidebar-buttons">
+            <Link to="/">
+              <PrimaryButton
+                text="HOME"
+                addIcon={true}
+                srcIcon={homeIcon}
+                srcHoverIcon={homeIconHover}
+              />
+            </Link>
             <PrimaryButton
-              text="HOME"
+              text="MODULES"
               addIcon={true}
-              srcIcon={homeIcon}
-              srcHoverIcon={homeIconHover}
+              srcIcon={modulesIcon}
+              srcHoverIcon={modulesIconHover}
+              onClickHandler={handleModulesButtonClick}
             />
-          </Link>
-          <PrimaryButton
-            text="MODULES"
-            addIcon={true}
-            srcIcon={modulesIcon}
-            srcHoverIcon={modulesIconHover}
-            onClickHandler={handleModulesButtonClick}
-          />
-          {isModulesListVisible && <ModulesList />}
-        </div>
-        <div className="user-area">
-          {/* <h3>
-            Welcome <span className="username">Guest</span>
-          </h3> */}
-          <Link to="/post-question">
-            <PrimaryButton
-              text="Ask new Question"
-              addIcon={true}
-              srcIcon={plusIcon}
-              srcHoverIcon={plusIconHover}
-            />
-          </Link>
-        </div>
-      </nav>
+            {isModulesListVisible && <ModulesList />}
+          </div>
+          <div className="ask-question">
+            <Link to="/post-question">
+              <PrimaryButton
+                text="Ask Question"
+                addIcon={true}
+                srcIcon={plusIcon}
+                srcHoverIcon={plusIconHover}
+                textContainerClassName="custom-text-container"
+              />
+            </Link>
+          </div>
+        </sidebar>
+      )}
     </>
   );
 };
