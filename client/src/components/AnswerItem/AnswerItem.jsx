ac39d2b145daed93f336672688d928f3125f12ac
zehra <zehrakocairi@gmail.com>
2024-02-23 16:39:35 +0100

    almost done

--- client/src/components/AnswerItem/AnswerItem.jsx
+++ client/src/components/AnswerItem/AnswerItem.jsx
@@ -1,17 +1,19 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 import "./AnswerItem.css";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
 import PropTypes from "prop-types";
 import { useAuth } from "../../contexts/AuthContext";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 
-const Answer = ({ answer, handleDelete, isAnswerBelongsToUser }) => {
+const Answer = ({ answer, handleDelete, isAnswerBelongsToUser, question }) => {
   const { user } = useAuth();
   const [isLiked, setIsLiked] = useState(false);
+  const [isApproved, setIsApproved] = useState(answer.isApproved ?? false);
   const [likeCount, setLikeCount] = useState(answer?.like_counter ?? 0);
   const { id } = useParams();
 
@@ -23,6 +25,10 @@ const Answer = ({ answer, handleDelete, isAnswerBelongsToUser }) => {
       day: "numeric",
     });
   };
+
+  useEffect(() => {
+    answer = { ...answer, isApproved };
+  }, [isApproved]);
 
   const { performFetch: fetchLikeCount } = useFetch(
     `/questions/${id}/answers/${answer._id}/like`,
@@ -45,28 +51,69 @@ const Answer = ({ answer, handleDelete, isAnswerBelongsToUser }) => {
     fetchLikeCount(options);
   };
 
+  const isQuestionBelongsToUser = question?.user_id === user?.id;
+
+  const { performFetch: fetchApproveValue } = useFetch(
+    `/questions/${id}/answers/${answer._id}/approve`,
+    (result) => {
+      setIsApproved(result.isApproved);
+    }
+  );
+
+  const handleApprove = () => {
+    const approve = {
+      current_user_id: user?.id ?? user?.name,
+      answer_owner_user_id: answer.user_id,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(approve),
+    };
+
+    fetchApproveValue(options);
+  };
+
   return (
     <div className="answer-wrapper">
       <div className="button-group">
         <Button
           className="icon-button"
+          disabled={
+            user && isQuestionBelongsToUser && !isAnswerBelongsToUser
+              ? false
+              : true
+          }
+          onClick={handleApprove}
+        >
+          {isApproved ? (
+            <StarBorderIcon style={{ fontSize: "18px", color: "#76f013" }} />
+          ) : (
+            <StarBorderIcon style={{ fontSize: "18px" }} />
+          )}
+        </Button>
+        <Button
+          className="icon-button"
           onClick={handleLike}
-          disabled={isAnswerBelongsToUser || !user ? true : false}
+          disabled={!user || isAnswerBelongsToUser ? true : false}
         >
           {isLiked ? (
             <ThumbUpIcon style={{ fontSize: "18px", color: "#76f013" }} />
           ) : (
             <ThumbUpIcon
               style={{ fontSize: "18px" }}
-              disabled={isAnswerBelongsToUser || !user ? true : false}
+              disabled={!user || isAnswerBelongsToUser ? true : false}
             />
           )}
         </Button>
         <Button
           className={
-            isAnswerBelongsToUser && user ? "icon-button delete" : "icon-button"
+            user && isAnswerBelongsToUser ? "icon-button delete" : "icon-button"
           }
-          disabled={isAnswerBelongsToUser && user ? false : true}
+          disabled={user && isAnswerBelongsToUser ? false : true}
           onClick={() => handleDelete(answer._id)}
         >
           <DeleteIcon style={{ fontSize: "18px" }} />
