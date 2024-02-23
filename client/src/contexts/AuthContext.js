51f10068c7223b355d216f86e87a62ddd064c83d
lenin del rio <lenindelrionicaragua@gmail.com>
2024-02-23 12:41:15 +0100

    Implemented user authentication and logout functionality

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,39 +1,39 @@
 import React, { createContext, useContext, useState } from "react";
 import PropTypes from "prop-types";
 import { logInfo } from "../../../server/src/util/logging";
+import useFetch from "../hooks/useFetch";
 
 const AuthContext = createContext();
 
+// Authentication provider component
 export const AuthProvider = ({ children }) => {
-  //I initial based on function and this function called only on initial render.
-  //because if we update UserState using UseEffect it will be updated after the initial render.
-  //otherwise the user will stay (null) after every time we reload the page.
+  const { performFetch } = useFetch("/auth/log-out", onLogoutSuccess);
 
+  // State to manage the user's authentication status
   const [user, setUser] = useState(() =>
     localStorage.getItem("user")
       ? JSON.parse(localStorage.getItem("user"))
       : null
   );
 
-  // When the component mounts, try to load the user from localStorage
-  // useEffect(() => {
-  //   const storedUser = localStorage.getItem("user");
-  //   if (storedUser) {
-  //     setUser(JSON.parse(storedUser));
-  //   }
-  // }, []);
-
-  // Function to handle user login
   const login = (userData) => {
     setUser(userData);
-    localStorage.setItem("user", JSON.stringify(userData)); // Save to localStorage upon login
+    localStorage.setItem("user", JSON.stringify(userData));
     logInfo("Login successful in AuthContext: " + JSON.stringify(userData));
   };
 
-  // Function to handle user logout
   const logout = () => {
     setUser(null);
-    localStorage.removeItem("user"); // Remove from localStorage upon logout
+    localStorage.removeItem("user");
+
+    performFetch({ method: "POST", url: "/auth/log-out" });
+  };
+
+  const onLogoutSuccess = () => {
+    // Clear the authentication context and localStorage upon receiving a successful response
+    setUser(null);
+    localStorage.removeItem("user");
+    logInfo("Logout successful in AuthContext");
   };
 
   return (
